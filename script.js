6f83431e3d0dff63ea119534c943a4763ceaaec5
atharva-aak <atharvakalbande5@gmail.com>
2025-02-01 15:48:55 +0530

    Added the css for website and also integrated the reset button

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const key = "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";
 const inputText = document.getElementById("input");
 const image = document.getElementById("image");
 const GenBtn = document.getElementById("btn");
+const svg = document.getElementById("svg");
+const load = document.getElementById("loading");
+const ResetBtn = document.getElementById("reset");
+
 
 async function query(data) {
 	const response = await fetch(
@@ -15,10 +19,14 @@ async function query(data) {
 		}
 	);
 	const result = await response.blob();
+	image.style.display = "block"
+	load.style.display="none";
 	return result;
 }
 
 async function generate() {
+
+	load.style.display="block";
 
     query().then((response) => {
 
@@ -31,11 +39,18 @@ async function generate() {
 
 GenBtn.addEventListener("click", () =>{
     generate();
+	svg.style.display = "none"
 });
 
 inputText.addEventListener("keydown", (e) =>{
     if(e.key == "Enter")
     {
         generate();
+		svg.style.display = "none"
     }
+})
+
+ResetBtn.addEventListener("click", ()=>{
+	inputText.value = ""
+	window.location.reload();
 })
